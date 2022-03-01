5a429c532c16b0f5b0680c8db2303afec322a1c4
Gisele Ribeiro <giselesousar@gmail.com>
2022-03-01 11:55:28 -0300

    implementing heatmap values normalization

--- src/utils/heatmap.js
+++ src/utils/heatmap.js
@@ -1,9 +1,9 @@
 const hue = 240;
 const saturation = 100;
 
-export function calculateRectColor(rootSize, rectSize) {
-    const rate = rectSize/rootSize;
-    const lightness = (rate < 1 ? 1 - rate : 0.3) * 95;
+export function calculateRectColor(rectHeatmap, heatmap) {
+    const value = ((rectHeatmap - heatmap.min) / (heatmap.max - heatmap.min));
+    const lightness = (1 - value) * 95;
     return {
         fill: `hsl(${hue}, ${saturation}%, ${lightness}%)`,
         color: lightness > 50 ? 'rgb(0,0,0)' : 'rgb(255,255,255)'
